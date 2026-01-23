f4cc2e3eb9c966a66aa8612907a69958dd8553f8
Marco Corradini <112563774+callmecorra@users.noreply.github.com>
2026-01-23 15:38:37 +0100

    Rename package to py-clob-client-fast and update version
    Updated package name and version for the fast fork of the client. Adjusted URLs and description accordingly.

--- setup.py
+++ setup.py
@@ -4,16 +4,16 @@ with open("README.md", "r", encoding="utf-8") as fh:
     long_description = fh.read()
 
 setuptools.setup(
-    name="py_clob_client",
-    version="0.34.5",
+    name="py-clob-client-fast",
+    version="0.34.5.post1",          
     author="Polymarket Engineering",
     author_email="engineering@polymarket.com",
     maintainer="Polymarket Engineering",
     maintainer_email="engineering@polymarket.com",
-    description="Python client for the Polymarket CLOB",
+    description="Python client for the Polymarket CLOB (fast fork)",
     long_description=long_description,
     long_description_content_type="text/markdown",
-    url="https://github.com/Polymarket/py-clob-client",
+    url="https://github.com/<YOUR_GH_USER_OR_ORG>/py-clob-client-fast",
     install_requires=[
         "eth-account>=0.13.0",
         "eth-utils>=4.1.1",
@@ -24,13 +24,14 @@ setuptools.setup(
         "httpx[http2]>=0.27.0",
     ],
     project_urls={
-        "Bug Tracker": "https://github.com/Polymarket/py-clob-client/issues",
+        "Bug Tracker": "https://github.com/<YOUR_GH_USER_OR_ORG>/py-clob-client-fast/issues",
+        "Upstream": "https://github.com/Polymarket/py-clob-client",
     },
     classifiers=[
         "Programming Language :: Python :: 3",
         "License :: OSI Approved :: MIT License",
         "Operating System :: OS Independent",
     ],
-    packages=setuptools.find_packages(),
+    packages=setuptools.find_packages(),  # will include py_clob_client_fast
     python_requires=">=3.9.10",
 )
